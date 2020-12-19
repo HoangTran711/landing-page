59926b702b4ec83e24c805b074e7c5aa73fced7e
Hoang Tran <tranhoang.finizz@gmail.com>
2020-12-19 20:02:39 +0700

    Replace: Replaced on Home Page

--- src/pages/home/home-page.js
+++ src/pages/home/home-page.js
@@ -2,7 +2,6 @@ import React from 'react'
 import cube from '../assets/cube.png'
 import './home-page.css'
 import classNames from 'classnames'
-import Build from '../assets/build.zip'
 
 
 export const HomePage = () => {
@@ -14,8 +13,8 @@ export const HomePage = () => {
                         <h1 className="title">Because <span className="privacy-text">privacy</span> matters</h1>
                         <h4 className="subtitle">Store, send, receive, swap anonymously.</h4>
                         <div className="btn-group" role="group" aria-label="Basic example">
-                            <button type="button" className={classNames("btn btn-primary")}><a href={Build} target="_blank" rel="noopener noreferrer" download>Get Extension</a></button>
-                            <button type="button" className={classNames("btn btn-link")}><a href="https://github.com/lightshadowbox" target="_blank" rel="noreferrer" >Read docs</a></button>
+                            <button type="button" className={classNames("btn btn-primary")}><a href="https://chrome.google.com/webstore/detail/lsb-wallet/bmoeljkahcfgehdkfkeafinpnfmkabda" target="_blank" rel="noopener noreferrer" download>Get Extension</a></button>
+                            <button type="button" className={classNames("btn btn-link")}><a href="https://lightshadowbox.medium.com/lsb-wallet-extension-the-first-gateway-to-crypto-privacy-f38bf82bc89c" target="_blank" rel="noreferrer" >Read docs</a></button>
                         </div>
                     </div>
                     <div className="banner-info__cube">
